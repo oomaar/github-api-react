5a6f242b31ea8307f007316dfcceae1597c8e160
Omar <omaar_5@hotmail.com>
2021-05-01 01:55:59 +0200

    feat: navbar part-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { 
+  Application,
+  Navbar,
+} from "./styledApp";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [followers, setFollowers] = useState("");
+  const [following, setFollowing] = useState("");
+  const [repos, setRepos] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [error, setError] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Application>
+      <Navbar>Github Search</Navbar>
+    </Application>
   );
-}
+};
 
 export default App;
---
+++ src/styledApp.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro';
+
+export const Application = styled.div``;
+
+export const Navbar = styled.nav`
+    padding: 20px;
+    background-color: rgb(223, 224, 222);
+`;
